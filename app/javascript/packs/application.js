1a2d1e4756e7f531e3f91ec8da93787d4cc73c1e
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-19 11:46:20 -0500

    Configure Bootstrap Popovers and Tooltips

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,15 @@ import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 
 import "../stylesheets/application.scss"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    });
+
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+});
