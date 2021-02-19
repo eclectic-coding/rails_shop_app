d17d927c6572d6bdee94af38ce817ca168f5d06c
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-19 13:14:47 -0500

    Add user gravatar to navbar with active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_person_name
+  has_one_attached :avatar
 
   extend FriendlyId
   friendly_id :name, use: :slugged
